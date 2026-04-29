cd6284a9d0eaf56019d9fbb54de04186b6b8e6e0
MasterOz786 <hassanejaz400@gmail.com>
2026-04-30 03:21:48 +0500

    Track fakenewsnet text dataset files with DVC and commit DVC metadata

--- datasets/scripts/isotfakenews-download.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("csmalarkodi/isot-fake-news-dataset", output_dir='./datasets/isotfakenews')
-
-print("Path to dataset files:", path)
--- datasets/scripts/liar-dataset-download.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("doanquanvietnamca/liar-dataset", output_dir="./datasets/liar-dataset")
-
-print("Path to dataset files:", path)
